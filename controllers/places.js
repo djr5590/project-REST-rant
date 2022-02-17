bc683db10afb7e97fbe73422dca339b794c26c17
Don Reeves <donjreeves@gmail.com>
2022-02-16 20:38:48 -0500

    Added Edit and Delete Buttons

--- controllers/places.js
+++ controllers/places.js
@@ -21,35 +21,33 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
+// GET PLACES
+router.get("/", (req, res) => {
+    // let places = [
+    //     {
+    //         name: "H-Thai-ML",
+    //         city: "Seattle",
+    //         state: "WA",
+    //         cuisines: "Thai, Pan-Asian",
+    //         pic: '/images/red-curry-chicken.jpg',
+    //         // Photo by <a href="https://unsplash.com/@grisskitchen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Max Griss</a> on <a href="https://unsplash.com/s/photos/thai-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    //     },
+    //     {
+    //         name: "Coding Cat Cafe",
+    //         city: "Phoenix",
+    //         state: "AZ",
+    //         cuisines: "Coffee, Bakery",
+    //         pic: '/images/coding-cat-cafe.jpg',
+    //         // Photo by <a href="https://unsplash.com/@herlifeinpixels?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Hannah Wei</a> on <a href="https://unsplash.com/s/photos/cat-cafe?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    
+    //     },
+    // ]
+    res.render("places/index", { places })
+})
 
 //GET NEW PLACES
 router.get('/new', (req, res) => {
     res.render('places/new')
-})
-
-
-// GET PLACES
-router.get("/", (req, res) => {
-    let places = [
-        {
-            name: "H-Thai-ML",
-            city: "Seattle",
-            state: "WA",
-            cuisines: "Thai, Pan-Asian",
-            pic: '/images/red-curry-chicken.jpg',
-            // Photo by <a href="https://unsplash.com/@grisskitchen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Max Griss</a> on <a href="https://unsplash.com/s/photos/thai-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-        },
-        {
-            name: "Coding Cat Cafe",
-            city: "Phoenix",
-            state: "AZ",
-            cuisines: "Coffee, Bakery",
-            pic: '/images/coding-cat-cafe.jpg',
-            // Photo by <a href="https://unsplash.com/@herlifeinpixels?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Hannah Wei</a> on <a href="https://unsplash.com/s/photos/cat-cafe?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-            
-        },
-    ]
-    res.render("places/index", { places })
 })
 
 router.get('/:id', (req, res) => {
@@ -61,7 +59,7 @@ router.get('/:id', (req, res) => {
         res.render('error404')
     }
     else{
-        res.render('places/show', { place: places[id]})
+        res.render('places/show', { place: places[id], id})
     }
 })
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const placesRoutes = require('./controllers/places')
+const methodOverride = require('method-override')
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use('/places', placesRoutes)
 
