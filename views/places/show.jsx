d646db6fb535177d02f8d11e96c675bb86577fd8
Don Reeves <donjreeves@gmail.com>
2022-02-27 13:26:48 -0500

    added show established, updated CSS

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,37 +1,45 @@
 const React = require('react')
 const Def = require('../default')
 
-function show (data) {
+function show(data) {
     return (
         <Def>
             <main>
                 <div className="row">
                     <div className="col-sm-6">
                         <img className="m-4" src={data.place.pic} alt={data.place.name} />
+                        <h4 className="text-center">
+                            Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}
+                        </h4>
                     </div>
                     <div className="col-sm-6">
-                        <h2 className="m-4">{ data.place.name }</h2>
-                        <h3>Rating</h3>
-                            <p>Not Rated</p>
-                        <h3>Description</h3>
-                            <p className="text-center">
-                            Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}                
-                            </p>
+                        <h1 className="m-4">{data.place.name}</h1>
+                        <h2>Rating</h2>
+                        <p>Not Rated</p>
+                        <h2>Description</h2>
+                        <h3>{data.place.showEstablished()}</h3>
+                        <h5>Serving {data.place.cuisines}</h5>
+                    </div>
+                </div>
+                <div class="row">
+                    <div>
+                        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                            Edit
+                        </a>
+                    </div>
+                    <div>
+                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                            <button type="submit" className="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>
                     </div>
                 </div>
                 <div className="mb-3">
-                    <h3>Comments</h3>
-                        <p>No comments yet!</p>
-                </div> 
+                    <h2>Comments</h2>
+                    <p>No comments yet!</p>
+                </div>
             </main>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                Edit
-            </a>  
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">
-                    Delete
-                </button>
-            </form>     
         </Def>
     )
 }
